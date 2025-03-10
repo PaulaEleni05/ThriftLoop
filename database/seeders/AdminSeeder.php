eabb6a578f5dbe3cd2439576c39eb02b1108bd03
Your Name <you@example.com>
2025-03-10 12:51:51 +0000

    Generate Model, Migration, Controller and Seeder for donations

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -12,6 +12,11 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Admmin User',
+            'email' => 'admin' . time() . '@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
     }
 }
